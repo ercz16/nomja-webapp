b71fa1b7cfa12f87edc16b4620f0541e25eff699
Ryan McCauley <ryanmcly@gmail.com>
2021-06-24 00:40:57 -0400

    Fix Delete Program

--- pages/manage.tsx
+++ pages/manage.tsx
@@ -159,7 +159,7 @@ const Manage = (props) => {
                 <CreateModal user={user} isOpen={createOpen} open={() => setCreateOpen(true)} close={() => setCreateOpen(false)} />
                 <div className="grid grid-cols-5 gap-4 py-8">
                     { user.programs.map((program) => {
-                        return <ProgramCard key={program.id} program={program} />
+                        return <ProgramCard key={program.id} program={program} user={user} />
                     })}
                 </div>
             </div>
@@ -169,7 +169,7 @@ const Manage = (props) => {
 }
 
 const ProgramCard = (props) => {
-    const { program } = props
+    const { program, user } = props
 
     const router = useRouter()
 
@@ -185,7 +185,7 @@ const ProgramCard = (props) => {
                 <div className="relative grid items-center grid-cols-3">
                     <p className="col-span-2 text-xl font-medium">{program.name}</p>
                     <div className={!optionsOpen ? "hidden" : "bg-white absolute right-0 col-span-1 p-2 border border-gray-300 text-sm text-gray-800 hover:text-gray-900 rounded mt-11"}>
-                        <a onClick={() => deleteProgram(program.id)} className="flex flex-row items-center gap-1 p-1 font-medium rounded cursor-pointer hover:bg-gray-100">
+                        <a onClick={() => deleteProgram(user, program.id)} className="flex flex-row items-center gap-1 p-1 font-medium rounded cursor-pointer hover:bg-gray-100">
                             Delete
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-auto" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
